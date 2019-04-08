c1594c3fe0b1cd9c793bd7fdae68d2d47bba42da
Anastasia Repina <anastasiya.repina2012@yandex.ru>
2019-04-08 19:31:23 +0300

    Fixed the buttons

--- src/components/GalaxyButtons.js
+++ src/components/GalaxyButtons.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../css/GalaxyButtons.css';
 
 class GalaxyButtons extends Component {
   constructor () {
@@ -15,7 +16,7 @@ class GalaxyButtons extends Component {
       this.setState({
           isDC: this.state.isMarvel,
           isMarvel: this.state.isDC
-      });     
+      });
       this.props.onGalaxyFilter(this.state.isMarvel)
   }
 
@@ -27,12 +28,10 @@ class GalaxyButtons extends Component {
     return (
       <center>
         <div if="galaxy" style={{display: 'inline-block',width:'100vw',marginTop:'10px'}}>
-          <button disabled={this.state.isDC}>
-            <img border="0" id="dc" src="dc.png" alt="dc" onClick={this.handleClick} style={style}/>
-          </button>
-          <button disabled={this.state.isMarvel}>
-            <img border="0" id="marvel" src="marvel.png" alt="marvel" onClick={this.handleClick} style={style}/>
-          </button>
+            <input type="image" disabled={this.state.isDC} border="0" id="dc" src="dc.png" alt="dc" onClick={this.handleClick} style={style}/>
+
+            <input type="image" disabled={this.state.isMarvel} border="0" id="marvel" src="marvel.png" alt="marvel" onClick={this.handleClick} style={style}/>
+
         </div>
       </center>
     );
